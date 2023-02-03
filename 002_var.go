70e1d0f0f0e845c909614e05a1aaa67bd33d4a37
姜峥嵘 <zhengrong.jiang@chiefclouds.com>
2023-02-03 12:54:47 +0800

    feat: add the new variable example based on Go

--- 002_var.go
+++ 002_var.go
@@ -8,5 +8,12 @@ var packageVar string = "package variable"
 
 func main() {
 	var funcVar string = "function variable"
+
+	{
+		var blockVar string = "block variable"
+		fmt.Println(packageVar, funcVar, blockVar)
+
+	}
+
 	fmt.Println(packageVar, funcVar)
 }
