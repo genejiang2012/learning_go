3c657b9c429dc912f5f02a0fb2b377ad355dbbed
genejiang2012 <genejiang2012@outlook.com>
2023-02-20 19:59:14 +0800

    use go mod to manage the tools

---
+++ src/demo/calc.go
@@ -0,0 +1,16 @@
+package calc
+
+var Age = 20
+var age = 10
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func Sub(x, y int) int {
+	return x - y
+}
+
+func add(x, y int) int {
+	return x + y
+}
---
+++ src/demo/tools.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"demo/calc"
+	"fmt"
+)
+
+func main() {
+	sum := calc.Add(1, 2)
+	fmt.Println("The sum is", sum)
+}
