bc9de417244840d219885742be3951c51174aa72
genejiang2012 <genejiang2012@outlook.com>
2023-03-03 18:53:55 +0800

    feat: add the files

---
+++ src/ch7_array/0303-max-value.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println()
+
+	var intArray = [...]int{-1, 1999, 99, 20, 199}
+
+	sum := 0
+	count := 0
+	var maxValue int
+	maxValue = intArray[0]
+
+	for i := 0; i < len(intArray); i++ {
+		if maxValue <= intArray[i] {
+			maxValue = intArray[i]
+		}
+		count = count + i
+		sum += intArray[i]
+	}
+
+	fmt.Printf("The max values is %d, the average value is %d", maxValue, sum/count)
+
+	fmt.Printf("The max values is %d, the average value is %.2f", maxValue, float64(sum)/float64(len(intArray)))
+
+}
